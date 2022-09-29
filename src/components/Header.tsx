cef57d166bdda8a9713245e1373007e165069750
Grégori Sabel <seriosabel57@gmail.com>
2022-09-29 10:17:42 -0300

    help modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Button, Flex, Text, Tooltip } from '@chakra-ui/react'
+import React, { useState } from "react";
+import { Button, Text, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Tooltip, useDisclosure } from '@chakra-ui/react'
 import { Stats } from "./Stats.tsx";
-import { BsFillGearFill, BsFillPieChartFill } from "react-icons/bs";
+import { BsQuestionCircleFill, BsFillPieChartFill } from "react-icons/bs";
 
 
 interface HeaderProps{
@@ -11,6 +11,8 @@ interface HeaderProps{
 
 export function Header({ formatTime, resultList }:HeaderProps){
   const [ showStats, setShowStats ] = useState(false)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
 
   
   return (
@@ -36,10 +38,29 @@ export function Header({ formatTime, resultList }:HeaderProps){
         </Tooltip>                      
 
         <Flex justify='flex-end' w='200px' pt='20px'>
-          <Flex opacity='0.5' _hover={{ opacity:'1' }} m='0' p='0'>
-            <BsFillGearFill size='24px'/>
+          <Flex opacity='0.5' _hover={{ opacity:'1' }} m='0' p='0' onClick={onOpen}>
+            <BsQuestionCircleFill size='24px'/>
           </Flex>
         </Flex>
+
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Ajuda</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody py='40px'>
+            <Text>Segure a tecla <strong>espaço</strong> por 0.5s para começar.</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Fechar
+            </Button>
+            {/* <Button variant='ghost'>Secondary Action</Button> */}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>        
 
       </Flex>
 
